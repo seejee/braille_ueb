d6eda634aaef3a12a7a45636f0054ac83af6afdd
Chris Geihsler <chris@geihsler.net>
2012-09-15 17:41:30 -0400

    removing requires for RM

--- lib/braille_ueb.rb
+++ lib/braille_ueb.rb
@@ -1,7 +1,3 @@
-require 'braille_ueb/constants' unless defined?(Motion::Project::Config)
-require 'braille_ueb/char' unless defined?(Motion::Project::Config)
-require 'braille_ueb/word' unless defined?(Motion::Project::Config)
-
 module BrailleUEB
 
   def self.translate(input)
