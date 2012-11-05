c5dbb0f4cd9f123f2dcd6e9228ae895dc11c8a2c
Chris Geihsler <chris@geihsler.net>
2012-11-04 19:27:04 -0500

    converting to utf source files

--- spec/braille_ueb_spec.rb
+++ spec/braille_ueb_spec.rb
@@ -1,45 +1,46 @@
+#encoding: UTF-8
 require_relative '../lib/braille_ueb'
 
 describe BrailleUEB do
 
-  it "should convert the alphabet" do
-    verify "a", "\u2801"
-    verify "b", "\u2803"
-    verify "c", "\u2809"
+  it 'should convert the alphabet' do
+    verify 'a', '⠁'
+    verify 'b', '⠃'
+    verify 'c', '⠉'
   end
 
-  it "should convert capital letters" do
-    verify "Z", "\u2820\u2835"
+  it 'should convert capital letters' do
+    verify 'Z', '⠠⠵'
   end
 
-  it "should convert single numbers" do
-    verify "1", "\u283c\u2801"
-    verify "2", "\u283c\u2803"
-    verify "3", "\u283c\u2809"
+  it 'should convert single numbers' do
+    verify '1', '⠼⠁'
+    verify '2', '⠼⠃'
+    verify '3', '⠼⠉'
   end
 
-  it "should only put the number prefix for the the first number" do
-    verify "12", "\u283c\u2801\u2803"
+  it 'should only put the number prefix for the the first number' do
+    verify '12', '⠼⠁⠃'
   end
 
-  it "should convert a period to a full stop" do
-    verify "Z.", "\u2820\u2835\u2832"
+  it 'should convert a period to a full stop' do
+    verify 'Z.', '⠠⠵⠲'
   end
 
-  it "should convert a comma" do
-    verify ",", "\u2802"
+  it 'should convert a comma' do
+    verify ',', '⠂'
   end
 
-  it "should perform contractions" do
-    verify "like", "\u2807"
+  it 'should perform contractions' do
+    verify 'like', '⠇'
   end
 
-  it "should perform contractions that end with punctuation" do
-    verify "like.", "\u2807\u2832"
+  it 'should perform contractions that end with punctuation' do
+    verify 'like.', '⠇⠲'
   end
 
-  it "should captialize contractions" do
-    verify "Like", "\u2820\u2807"
+  it 'should captialize contractions' do
+    verify 'Like', '⠠⠇'
   end
 
   def verify(english, braille)
